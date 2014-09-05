727cc2a01475f12fb65f723333a066d46778b8b1
David Flores <dmousex@gmail.com>
2014-09-05 00:08:26 -0500

    initial commit

---
+++ src/CorsOptionsRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\cors\CorsOptionsRequest.
+ */
+
+namespace Drupal\cors;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use \Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use \Symfony\Component\HttpFoundation\Response;
+
+class CorsOptionsRequest implements EventSubscriberInterface {
+
+  static public function getSubscribedEvents() {
+    return [
+      'kernel.request' => 'onKernerRequest',
+    ];
+  }
+
+  public function onKernerRequest(GetResponseEvent $event){
+    $request = $event->getRequest();
+
+    if ($request->getMethod() == 'OPTIONS') {
+      $response = new Response();
+      $response->setStatusCode(200);
+      $response->headers->set('Access-Control-Allow-Credentials', 'true');
+      $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+      $event->setResponse($response);
+    }
+  }
+}

731d7b133ac6bc59d9f670e3bf65ed8196c85c04
David Flores <dmousex@gmail.com>
2014-09-08 06:07:08 -0500

    set doc block

--- src/StackOptionsRequest.php
+++ src/StackOptionsRequest.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @file 
+ *   Contains Drupal\cors\StackOptionsRequest.
+ */
+
 namespace Drupal\cors;
 
 use Drupal\Core\DrupalKernelInterface;
@@ -7,12 +12,31 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Wrapper to OPTIONS request.
+ */
 class StackOptionsRequest implements HttpKernelInterface {
 
-  public function __construct(HttpKernelInterface $http_kernel) {
-    $this->httpKernel = $http_kernel;
+  /**
+   * The wrapped kernel implementation.
+   *
+   * @var \Symfony\Component\HttpKernel\HttpKernelInterface
+   */
+  private $app;
+
+  /**
+   * Create a new StackOptionsRequest instance.
+   *
+   * @param  \Symfony\Component\HttpKernel\HttpKernelInterface  $app
+   *   Hrrp Kernel.
+   */
+  public function __construct(HttpKernelInterface $app) {
+    $this->app = $app;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = TRUE) {
 
     if ($request->getMethod() == 'OPTIONS') {
